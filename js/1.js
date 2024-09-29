fd0008c370bb59c7b39d0293aab66c1ba62b9ca1
Miillla <p.milana0510@gmail.com>
2024-09-30 00:16:03 +0300

    update

--- js/1.js
+++ js/1.js
@@ -1,59 +1,30 @@
-// class Calculator {
-//   constructor() {
-//     this.value = 0;
-//   }
-//   //   add(5, 3)
-//   add(a, b) {
-//     return a + b;
-//   }
-//   subtract(a, b) {
-//     return a - b;
-//   }
-//   multiply(a, b) {
-//     return a * b;
-//   }
-//   divide(a, b) {
-//     //  Can't divede by 0
-//     if (0 === b) {
-//       return;
-//     }
-//     return a / b;
-//   }
-// }
+class Calculator {
+  constructor() {
+    this.value = 0;
+  }
+  //   add(5, 3)
+  add(a, b) {
+    return a + b;
+  }
+  subtract(a, b) {
+    return a - b;
+  }
+  multiply(a, b) {
+    return a * b;
+  }
+  divide(a, b) {
+    //  Can't divede by 0
+    if (0 === b) {
+      return;
+    }
+    return a / b;
+  }
+}
 
-// const calc = new Calculator();
+const calc = new Calculator();
 
-// console.log(calc.add(5, 3)); // 8
+console.log(calc.add(5, 3)); // 8
 
-// console.log(calc.subtract(10, 4)); // 6
+console.log(calc.subtract(10, 4)); // 6
 
-// console.log(calc.multiply(3, 6)); // 18
-
-// console.log(calc.divide(8, 2)); // 4
-// console.log(calc.divide(8, 0)); // 4
-// // class Person {
-// //   constructor(first, last) {
-// //     this.first = first;
-// //     this.last = last;
-// //   }
-// //   get fullname() {
-// //     return `${this.first} ${this.last}`;
-// //   }
-// //   get firstname() {
-// //     return this.first;
-// //   }
-// //   setAge(age) {
-// //     this.age = age;
-// //     return this;
-// //   }
-// //   setFirst(first) {
-// //     this.first = first;
-// //     return this;
-// //   }
-// // }
-
-// // const doctor = new Person("Mary", "kkkk");
-// // // doctor.setAge(12);
-// // // doctor.setFirst("Piter");
-// // doctor.setFirst("Philipa").setAge(12).setFirst("Mary");
-// // console.log(doctor.first, doctor.age);
+console.log(calc.multiply(3, 6)); // 18
